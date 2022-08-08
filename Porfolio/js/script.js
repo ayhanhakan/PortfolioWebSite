0490659427e33461b6642930edd0c1cb5efc7ce1
ayhanhakan <ayhanhakantekir@gmail.com>
2022-08-08 21:17:30 +0300

    v1.1

--- Porfolio/js/script.js
+++ Porfolio/js/script.js
@@ -1,62 +1,61 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
-  
-    $(window).on('scroll load',function(){
-  
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('toggle');
-  
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
-  
-    });
-  
-    // smooth scrolling 
-  
-    $('a[href*="#"]').on('click',function(e){
-  
-      e.preventDefault();
-  
-      $('html, body').animate({
-  
-        scrollTop : $($(this).attr('href')).offset().top,
-  
-      },
-        500, 
-        'linear'
-      );
-  
-    });
-  
-  });
+//     $('#menu').click(function(){
+//       $(this).toggleClass('fa-times');
+//       $('header').toggleClass('toggle');
+//     });
 
-  let cursor1 = document.querySelector('.cursor-1');
-  let cursor2 = document.querySelector('.cursor-2');
+//     $(window).on('scroll load',function(){
 
-  window.onmousemove = (e) => {
+//       $('#menu').removeClass('fa-times');
+//       $('header').removeClass('toggle');
+
+//       if($(window).scrollTop() > 0){
+//         $('.top').show();
+//       }else{
+//         $('.top').hide();
+//       }
+
+//     });
+
+//     // smooth scrolling 
+
+//     $('a[href*="#"]').on('click',function(e){
+
+//       e.preventDefault();
+
+//       $('html, body').animate({
+
+//         scrollTop : $($(this).attr('href')).offset().top,
+
+//       },
+//         500, 
+//         'linear'
+//       );
+
+//     });
+
+//   });
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) => {
     cursor1.style.top = e.pageY + 'px';
     cursor1.style.left = e.pageX + 'px';
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';
-  }
+}
 
-document.querySelectorAll('a').forEach(links =>{
+document.querySelectorAll('a').forEach(links => {
 
-  links.onmouseenter = () => {
-    cursor1.classList.add('a');
-    cursor2.classList.add('a');
-  }
+    links.onmouseenter = () => {
+        cursor1.classList.add('a');
+        cursor2.classList.add('a');
+    }
 
-  links.onmouseleave = () => {
-    cursor1.classList.remove('a');
-    cursor2.classList.remove('a');
-  }
+    links.onmouseleave = () => {
+        cursor1.classList.remove('a');
+        cursor2.classList.remove('a');
+    }
 });
-
